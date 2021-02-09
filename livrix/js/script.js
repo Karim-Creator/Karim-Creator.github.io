cf487c8666e49ff01275194fe81e52d0ac22cfa3
karim <develkarim@gmail.com>
2021-02-09 14:36:25 +0100

    livrix Added

---
+++ livrix/js/script.js
@@ -0,0 +1,18 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+//Scroll Animation
+AOS.init({
+    offset: 000, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000 // values from 0 to 3000, with step 50ms
+});
